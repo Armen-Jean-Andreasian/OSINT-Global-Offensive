fa78f4d48d6c1556959324c2547416be41cd39d6
Armen-Jean Andreasian <armen_andreasian@proton.me>
2024-12-21 09:31:11 +0700

    fixed scrollbar issue

--- obtained_data_app/urls.py
+++ obtained_data_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'obtained_data_app'
+
 urlpatterns = [
     path('<uuid:dynamic_id>/', views.redirect_to_destination, name='redirect_to_destination'),
     path('gathered_data/<uuid:logger_id>/', views.display_obtained_data, name='gathered_data'),
--- project/urls.py
+++ project/urls.py
@@ -1,3 +1,5 @@
+# project/urls.py
+
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.shortcuts import redirect
@@ -6,9 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('auth_app.urls')),
     path('profile/', include('dashboard_app.urls')),
-
-    path('obtain_data/', include('obtained_data_app.urls')),
-
-    # redirect all other paths to login
+    path('obtain_data/', include('obtained_data_app.urls', namespace='obtained_data_app')),  # Add namespace here
     re_path(r'^(?!obtain_data).*$', lambda request: redirect('auth_app:login'))
 ]
