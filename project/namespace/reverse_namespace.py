92d41aa8ae074d5c27df7296906adf08f0bfdf2f
Armen-Jean Andreasian <armen_andreasian@proton.me>
2024-12-21 07:12:53 +0700

    Obtained Data page is defined correctly, connected

--- project/namespace/reverse_namespace.py
+++ project/namespace/reverse_namespace.py
@@ -2,3 +2,4 @@ class ReverseNamespace:
     login = 'auth_app:login'
     register = 'auth_app:register'
     dashboard = 'dashboard_app:dashboard'
+    gathered_data = 'obtained_data_app:gathered_data'
--- project/urls.py
+++ project/urls.py
@@ -1,23 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.shortcuts import redirect
-from django.conf import settings
-from django.conf.urls.static import static
-import os
-
-
-def redirect_to_login(request):
-    return redirect('auth_app:login')
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('auth_app.urls')),
     path('profile/', include('dashboard_app.urls')),
-    path('obtain_data/', include('obtained_data_app.urls')),
-    re_path(r'^(?!obtain_data).*$', redirect_to_login),
-    # match all other (undefined) paths and redirecting to login page, except for those starting with obtain_data
-]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, 'shared_templates'))
+    path('obtain_data/', include('obtained_data_app.urls')),
+
+    # redirect all other paths to login
+    re_path(r'^(?!obtain_data).*$', lambda request: redirect('auth_app:login'))
+]
